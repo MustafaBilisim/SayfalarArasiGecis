273e82aeb5df59114032b2a60d721332ddb8a910
Proje5 <arikmustafa5015@gmail.com>
2025-03-07 16:28:26 +0300

    ilk yukleme

---
+++ app/src/main/java/com/example/sayfalararasigecis/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.sayfalararasigecis
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sayfalararasigecis.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun Gecis(view: View)
+    {
+        var gec = Intent(this,ActivitySayfa2::class.java)
+        startActivity(gec)
+    }
+}
